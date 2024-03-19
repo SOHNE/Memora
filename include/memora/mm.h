75166c2df9034f31aef05a14c42b8ca37a3ddedf
Leandro Peres <leandroperes@protonmail.com>
2024-03-19 20:48:03 -0300

    Fixed calculations

--- include/memora/mm.h
+++ include/memora/mm.h
@@ -7,18 +7,18 @@
 
 typedef struct vm_page_family_t
 {
-    char struct_name[MM_MAX_STRUCT_NAME];
-    uint32_t struct_size;
+  char struct_name[MM_MAX_STRUCT_NAME];
+  uint32_t struct_size;
 } vm_page_family_t;
 
 typedef struct vm_page_for_families_t
 {
-    struct vm_page_for_families_ *next;
-    vm_page_family_t vm_page_family[0]; 
-} vm_page_for_families_t; 
+  struct vm_page_for_families_ *next;
+  vm_page_family_t vm_page_family[0];
+} vm_page_for_families_t;
 
-#define MAX_FAMILIES_PER_VM_PAGE    \
-    SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t*)
+#define MAX_FAMILIES_PER_VM_PAGE                                               \
+  (SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t *)) /                      \
+      sizeof(vm_page_family_t)
 
 #endif // MEMORA_MM_H
-
