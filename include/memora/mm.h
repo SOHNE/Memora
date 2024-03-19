f70af987234e247737bd6693af8f5c9adb4763fe
Leandro Peres <leandroperes@protonmail.com>
2024-03-19 20:26:34 -0300

    Creation of mm.h

--- include/memora/mm.h
+++ include/memora/mm.h
@@ -1,7 +1,24 @@
 #ifndef MEMORA_MM_H
 #define MEMORA_MM_H
 
+#include <stdint.h> // uint32_t
 
+#define MM_MAX_STRUCT_NAME 32
+
+typedef struct vm_page_family_t
+{
+    char struct_name[MM_MAX_STRUCT_NAME];
+    uint32_t struct_size;
+} vm_page_family_t;
+
+typedef struct vm_page_for_families_t
+{
+    struct vm_page_for_families_ *next;
+    vm_page_family_t vm_page_family[0]; 
+} vm_page_for_families_t; 
+
+#define MAX_FAMILIES_PER_VM_PAGE    \
+    SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t*)
 
 #endif // MEMORA_MM_H
 
--- include/memora/uapi_mm.h
+++ include/memora/uapi_mm.h
@@ -1,8 +1,8 @@
 #ifndef UAPI_MM_H
 #define UAPI_MM_H
 
+#include <memora/mm.h>
 #include <stdio.h>
-
 
 /** Initializes the memory management system. */
 void mm_init();
@@ -12,10 +12,10 @@ size_t get_system_page_size();
 
 /**
  * Requests a new virtual memory page from the kernel.
- * 
+ *
  * Input:
  *  units The number of pages to be allocated.
- * 
+ *
  * Output:
  *  void *: A pointer to the allocated memory page.
  */
@@ -23,7 +23,7 @@ void *mm_get_new_vm_page_from_kernel(int units);
 
 /**
  * Returns a virtual memory page back to the kernel.
- * 
+ *
  * Input:
  *  void *vm_page: A pointer to the memory page to be returned.
  *  int units: The number of pages that the pointer represents.
@@ -31,4 +31,3 @@ void *mm_get_new_vm_page_from_kernel(int units);
 void m_return_vm_page_to_kernel(void *vm_page, int units);
 
 #endif // UAPI_MM_H
-
